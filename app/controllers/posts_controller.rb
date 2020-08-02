a725b82e6bdc964fbe3812170965f2286c3d0359
Daniel Marino <marinodanielj@hotmail.com>
2020-08-02 16:35:45 -0400

    App is set up with new post, edit forms. New buttons added to go between views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,39 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = User.first    # TODO: change once we have authentification
 
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to post_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end
