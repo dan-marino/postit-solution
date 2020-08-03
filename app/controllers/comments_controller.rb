573f9d2ba9a2d0c7f3d41b936273bb8d2e0b99d7
Daniel Marino <marinodanielj@hotmail.com>
2020-08-03 11:44:32 -0400

    Set up comments to display on post page, refactored comment form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    # @comment = Comment.new(comment_params)
+    # @comment.post = @post
+    # The two lines of code above this are equivalent to the following line of code
+    @comment = @post.comments.build(comment_params)
+    @comment.creator = User.first   # TODO fix after authentification
+
+    if @comment.save
+      flash[:notice] = "Your comment was saved"
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
