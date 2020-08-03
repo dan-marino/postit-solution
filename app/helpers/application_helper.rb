a0dd25375e1643d39d9022f0c50418439a9a7f96
Daniel Marino <marinodanielj@hotmail.com>
2020-08-03 13:23:27 -0400

    added helpers to format date_time and url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?('http://') ? str : "http://" + str
+  end
+
+  def display_date_time(dt)
+    dt.strftime("%m/%d/%Y %I:%M%P %Z")
+  end
 end
