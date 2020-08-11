df3983145328f067b720e9873afb11927f6591d8
Daniel Marino <marinodanielj@hotmail.com>
2020-08-11 16:03:12 -0400

    Able to register new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get 'register', to: 'users#new'
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
@@ -11,4 +12,6 @@ PostitTemplate::Application.routes.draw do
   end
   resources :categories, only: [:new, :create, :show]
   # resources :creators, except: [:destroy]
+
+  resources :users, only: [:show, :create, :edit, :update]
 end
