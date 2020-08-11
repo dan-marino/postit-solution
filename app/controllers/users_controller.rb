dea12e3606dff2a83f05610349758ca2b0b17bf1
Daniel Marino <marinodanielj@hotmail.com>
2020-08-11 18:50:48 -0400

    User dropdown menu. Can edit and view user profile/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update, :create, :edit, :show]
+  before_action :require_same_user, only: [:edit, :udpate ]
+
   def new
     @user = User.new
   end
@@ -15,15 +18,19 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "You're profile was updated"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
+  def edit
   end
 
   def show
-
-  end
-
-  def update
 
   end
 
@@ -31,5 +38,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that"
+      redirect_to root_path
+    end
   end
 end
